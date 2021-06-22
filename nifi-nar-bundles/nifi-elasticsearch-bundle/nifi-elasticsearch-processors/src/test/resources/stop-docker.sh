1d1788cc9f8d01238857e745dc88a4b70b06ad8c
mantasheva-dzh <mds@arenadata.io>
2021-06-22 16:24:55 +0300

    fixing yml-files

--- nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/test/resources/stop-docker.sh
+++ nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/test/resources/stop-docker.sh
@@ -1,11 +1,11 @@
-docker stop ES01
-docker stop ES02
-docker stop SQUID
-docker stop SQUID_AUTH
-docker rm ES01
-docker rm ES02
-docker rm SQUID
-docker rm SQUID_AUTH
+docker stop ES_NIFI_01
+docker stop ES_NIFI_02
+docker stop SQUID_NIFI
+docker stop SQUID_NIFI_AUTH
+docker rm ES_NIFI_01
+docker rm ES_NIFI_01
+docker rm SQUID_NIFI
+docker rm SQUID_NIFI_AUTH
 docker volume rm data01
 docker volume rm data02
 docker network rm es_squid
