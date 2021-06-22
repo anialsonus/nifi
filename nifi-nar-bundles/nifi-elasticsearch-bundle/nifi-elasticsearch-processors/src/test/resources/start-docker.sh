636dd4287b65cb9d9ea1c73672f4cd1b7d3803f9
mantasheva-dzh <mds@arenadata.io>
2021-06-22 16:38:38 +0300

    fixing yml-files

--- nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/test/resources/start-docker.sh
+++ nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/test/resources/start-docker.sh
@@ -1,5 +1,5 @@
 docker network create --subnet=172.18.0.0/16 --gateway=172.18.0.1 es_squid
-docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" -e http.port=$3 -d -p $1:$3 -v $6/es1.yml:/usr/share/elasticsearch/config/elasticsearch.yml  -v data01:/usr/share/elasticsearch/data --network=es_squid --ip 172.18.0.2 --name ES_NIFI_01 elasticsearch:7.4.2
-docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" -e http.port=$3 -d -p $2:$3 -v $6/es2.yml:/usr/share/elasticsearch/config/elasticsearch.yml  -v data02:/usr/share/elasticsearch/data --network=es_squid --ip 172.18.0.3 --name ES_NIFI_02 elasticsearch:7.4.2
+docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" -e http.port=$3 -d -p $1:$3 -v $6/es1.yml:/usr/share/elasticsearch/config/elasticsearch.yml  -v es_nifi_01:/usr/share/elasticsearch/data --network=es_squid --ip 172.18.0.2 --name ES_NIFI_01 elasticsearch:7.4.2
+docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" -e http.port=$3 -d -p $2:$3 -v $6/es2.yml:/usr/share/elasticsearch/config/elasticsearch.yml  -v es_nifi_02:/usr/share/elasticsearch/data --network=es_squid --ip 172.18.0.3 --name ES_NIFI_02 elasticsearch:7.4.2
 docker run --name SQUID_NIFI -d --restart=always --publish $4:3228 --volume $6/squid.conf:/etc/squid/squid.conf  --volume /srv/docker/squid/cache:/var/spool/squid --network=es_squid sameersbn/squid:3.5.27-2
 docker run --name SQUID_NIFI_AUTH -d --restart=always --publish $5:3328 --volume $6/squid-auth.conf:/etc/squid/squid.conf  --volume /srv/docker/squid/cache:/var/spool/squid --network=es_squid sameersbn/squid:3.5.27-2
--- nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/test/resources/stop-docker.sh
+++ nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/test/resources/stop-docker.sh
@@ -6,6 +6,6 @@ docker rm ES_NIFI_01
 docker rm ES_NIFI_01
 docker rm SQUID_NIFI
 docker rm SQUID_NIFI_AUTH
-docker volume rm data01
-docker volume rm data02
+docker volume rm es_nifi_01
+docker volume rm es_nifi_02
 docker network rm es_squid
