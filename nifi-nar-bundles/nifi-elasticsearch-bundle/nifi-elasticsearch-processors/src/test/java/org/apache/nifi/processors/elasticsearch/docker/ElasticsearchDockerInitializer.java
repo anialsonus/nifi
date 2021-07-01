0bcbc8e542f9817bbc1fcfa2fe0ff253cd623369
mantasheva-dzh <mds@arenadata.io>
2021-07-01 15:06:32 +0300

    logs fix

--- nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/test/java/org/apache/nifi/processors/elasticsearch/docker/ElasticsearchDockerInitializer.java
+++ nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/test/java/org/apache/nifi/processors/elasticsearch/docker/ElasticsearchDockerInitializer.java
@@ -149,7 +149,7 @@ public class ElasticsearchDockerInitializer {
                     "Docker network used in elasticsearch & squid containers initialization - " + network + "\n" +
                     "The following ip addresses were set for elasticsearch nodes - "
                     + elasticsearchServerHosts.get(ElasticsearchNodesType.ES_NODE_01_IP_ADDRESS) + ":9200"
-                    + "\n" + elasticsearchServerHosts.get(ElasticsearchNodesType.ES_NODE_02_IP_ADDRESS) + ":9200"
+                    + "; " + elasticsearchServerHosts.get(ElasticsearchNodesType.ES_NODE_02_IP_ADDRESS) + ":9200"
                     +".\nThe following ip address was set for squid - " +  "localhost:" + elasticsearchSquidDockerServicesPorts.get(DockerServicePortType.SQUID_SP)
                     +".\nThe following ip address was set for squid with authentication parameters - " + "localhost:" + elasticsearchSquidDockerServicesPorts.get(DockerServicePortType.SQUID_AUTH_SP)
             );
